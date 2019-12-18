eb52a3da77c1a9f9cb6c60f113944dfbec7a6546
roskill <roskill76@gmail.com>
2019-12-18 14:48:10 +1100

    Create clock function, deploy using sam

---
+++ sam-app/.aws-sam/build/ClockFunction/handler.js
@@ -0,0 +1,8 @@
+const moment = require("moment");
+
+exports.clock = async event => {
+  console.log("Clock function ran");
+  const message = moment().format();
+  const response = { statusCode: 200, body: JSON.stringify(message) };
+  return response;
+};
